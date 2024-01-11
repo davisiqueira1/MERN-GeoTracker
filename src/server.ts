3f095383f07c590467836a7faed63bc861b2e6c1
Davi Siqueira <davisvsiqueira@gmail.com>
2024-01-11 09:54:52 -0300

    feat(db): connect to mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import { config } from "dotenv";
+import mongoose from "mongoose";
+
+config();
 
 const app = express();
 
 app.disable("x-powered-by");
+
+const { MONGO_HOST, MONGO_PORT, MONGO_DATABASE } = process.env;
+mongoose
+  .connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`)
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = 3000;
 
