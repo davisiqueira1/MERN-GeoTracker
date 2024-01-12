4950e58bdf7fca823d1db9ea2d8ffec1b04573ad
Davi Siqueira <davisvsiqueira@gmail.com>
2024-01-12 10:31:35 -0300

    fix: handle invalid id error in DeliverieService

--- src/services/DeliverieService.ts
+++ src/services/DeliverieService.ts
@@ -12,20 +12,24 @@ const createDeliverie = async (deliverie: CreateDeliverieType) => {
     await newDeliverie.save();
     return newDeliverie;
   } catch (err) {
-    console.log(err);
     throw new ApiError("Error on create deliverie", 400);
   }
 };
 
 const deleteDeliverie = async (id: string) => {
-  const deliverie = await Deliverie.findById(id);
+  const deliverie = await Deliverie.findById(id).catch((err) => {
+    throw new ApiError(
+      "Invalid id. It must be a 24 character hex string, 12 byte Uint8Array, or an integer",
+      400
+    );
+  });
   console.log(deliverie);
+
   if (!deliverie) throw new ApiError("Deliverie not found", 404);
 
   try {
     await deliverie.deleteOne();
   } catch (err) {
-    console.log(err);
     throw new ApiError("Error on delete deliverie", 400);
   }
 };
