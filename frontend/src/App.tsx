4428d959473ff7f6c820a7489c95cce6177c35c7
Davi Siqueira <davisvsiqueira@gmail.com>
2024-01-13 18:50:45 -0300

    feat: add leaflet placeholder map (not a component yet)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,32 @@
+import { MapContainer, Marker, TileLayer, Popup, useMap } from "react-leaflet";
+// useMap is a hook that gives access to the map object
+// docs: https://react-leaflet.js.org/docs/example-popup-marker/
+import "./App.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div id="container">
+      <MapContainer
+        id="map"
+        center={[-23.5, -46.5]}
+        zoom={17}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-23.5, -46.5]}>
+          <Popup>
+            {/* hardcoded example */}
+            Eduardo
+            <br />
+            120kg
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default App;
