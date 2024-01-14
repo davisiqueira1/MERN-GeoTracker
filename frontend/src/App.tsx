9876bba1823f23e9cd6971bcdf8b79e1e315923a
Davi Siqueira <davisvsiqueira@gmail.com>
2024-01-14 09:51:52 -0300

    feat: create non-styled textfields and buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,56 @@ import Map from "./components/Map";
 function App() {
   return (
     <div id="container">
-      <div id="form-container"></div>
+      <div id="form-container">
+        <form id="form">
+          <input
+            required
+            id="field"
+            placeholder="Name"
+            name="name"
+            disabled={false}
+          />
+          <br />
+          <input
+            required
+            id="field"
+            placeholder="Weight"
+            name="weight"
+            disabled={false}
+          />
+          <br />
+          <input
+            required
+            id="field"
+            placeholder="Address"
+            name="address"
+            disabled={false}
+          />
+          <br />
+          <input
+            required
+            id="field"
+            placeholder="Latitude"
+            name="lat"
+            disabled={true}
+          />
+          <input
+            required
+            id="field"
+            placeholder="Longitude"
+            name="lng"
+            disabled={true}
+          />
+          <br />
+          <input
+            id="botao-formulario"
+            type="submit"
+            value="Register customer"
+          />
+          <br />
+          <input id="botao-formulario" type="submit" value="Reset fields" />
+        </form>
+      </div>
       <div id="map-container">
         <Map />
         <div id="table"></div>
