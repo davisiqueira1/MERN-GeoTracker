6f5ecc53f412647044ccf2abde09ced788a4aca1
Davi Siqueira <davisvsiqueira@gmail.com>
2024-01-14 10:23:40 -0300

    feat: style text fields

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,15 @@ import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
-    <div id="container">
-      <div id="form-container">
-        <RegisterForm />
-      </div>
-      <div id="map-container">
-        <Map />
-        <div id="table"></div>
+    <div id="outer-container">
+      <div id="inner-container">
+        <div id="form-container">
+          <RegisterForm />
+        </div>
+        <div id="map-container">
+          <Map />
+          <div id="table"></div>
+        </div>
       </div>
     </div>
   );
