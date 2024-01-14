c3863e42638af5f0c58f11164fdf60a4d30a7e6a
Davi Siqueira <davisvsiqueira@gmail.com>
2024-01-14 10:01:36 -0300

    feat: componentize RegisterForm

---
+++ frontend/src/components/RegisterForm.tsx
@@ -0,0 +1,50 @@
+function RegisterForm() {
+  return (
+    <form id="form">
+      <input
+        required
+        id="field"
+        placeholder="Name"
+        name="name"
+        disabled={false}
+      />
+      <br />
+      <input
+        required
+        id="field"
+        placeholder="Weight"
+        name="weight"
+        disabled={false}
+      />
+      <br />
+      <input
+        required
+        id="field"
+        placeholder="Address"
+        name="address"
+        disabled={false}
+      />
+      <br />
+      <input
+        required
+        id="field"
+        placeholder="Latitude"
+        name="lat"
+        disabled={true}
+      />
+      <input
+        required
+        id="field"
+        placeholder="Longitude"
+        name="lng"
+        disabled={true}
+      />
+      <br />
+      <input id="botao-formulario" type="submit" value="Register customer" />
+      <br />
+      <input id="botao-formulario" type="submit" value="Reset fields" />
+    </form>
+  );
+}
+
+export default RegisterForm;
