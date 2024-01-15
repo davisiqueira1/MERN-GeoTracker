a4728c28c6614a5e3101535bea732d200175ba6a
Davi Siqueira <davisvsiqueira@gmail.com>
2024-01-15 09:12:36 -0300

    feat: split axios between backend and google api calls

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,4 +1,11 @@
 import axios from "axios";
 
-const PORT = 3000;
-export default axios.create({ baseURL: `http://localhost:${PORT}` });
+const googleAxios = axios.create({
+  baseURL: "https://maps.googleapis.com/maps/api/geocode",
+});
+
+const PORT = import.meta.env.VITE_BACKEND_PORT || 3000;
+const HOST = import.meta.env.VITE_BACKEND_HOST || "localhost";
+const backendAxios = axios.create({ baseURL: `http://${HOST}:${PORT}` });
+
+export { googleAxios, backendAxios };
