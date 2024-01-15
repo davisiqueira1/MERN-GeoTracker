7bf7a70359a830d85e2551ba0500b2f4e6ec3ae0
Davi Siqueira <davisvsiqueira@gmail.com>
2024-01-15 16:55:27 -0300

    feat: add register customer function

--- frontend/src/interface/IDelivery.ts
+++ frontend/src/interface/IDelivery.ts
@@ -1,20 +1,17 @@
-import { Key } from "react";
-
 interface IDelivery {
-  _id: Key;
   name: String;
-  weight: Number;
+  weight: number;
   address: {
     street?: String;
-    number?: Number;
+    number?: number;
     neighborhood?: String;
     complement?: String;
     city?: String;
     state?: String;
     country?: String;
     geolocation: {
-      latitude: Number;
-      longitude: Number;
+      latitude: number;
+      longitude: number;
     };
   };
 }
